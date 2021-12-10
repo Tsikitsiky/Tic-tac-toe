8c15df596dacc009fa5aba6903f1866b6eac1ad4
Tsikitsiky <sarah.raz@onja.org>
2021-12-10 15:29:04 +0300

    add more test

--- cypress/integration/allTest.spec.js
+++ cypress/integration/allTest.spec.js
@@ -1,5 +1,12 @@
 /// <reference types="cypress" />
 
+// Fake player choosing
+const selectPlayer = (playerChoice) => {
+    const playerId = playerChoice === 'X' ? 1 : 2;
+    cy.get(`[data-cy='player-${playerId}']`).should('have.text', playerChoice).click().then(() => {
+        cy.get('[data-cy="board"]').should('exist')
+    })
+}
 
 describe('test choose first player', () => {
     before(() => {
@@ -18,16 +25,53 @@ describe('test choose first player', () => {
         cy.get("[data-cy='button-wrapper']").find('button').contains(/X|O/g)
     })
 
-    it('should choose X as a first player and should display the board', () => {
+    it('should choose X as a first player and should display the board and remove choose player header', () => {
         cy.get("[data-cy='player-1']").should('have.text', 'X').click().then(() => {
             cy.get('[data-cy="board"]').should('exist')
+            cy.get("[data-cy='player-choice']").should('not.exist')
         })
     })
 
 })
 
+describe('should be able to choose first player', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+    })
+
+    it('should be able to choose O', () => {
+        selectPlayer('O');
+    })
+
+    it('should be able to choose X', () => {
+        selectPlayer('X');
+    })
+})
+
+describe('Duplicate clicks should not change square text', () => {
+    before(() => {
+        cy.visit('http://localhost:3000/');
+    })
+
+    it('click on a square twice do not change the text', () => {
+        selectPlayer('O');
+        // Click on square 2
+        cy.get("[data-cy='square']").eq(1).click().should('have.text', 'O')
+        // Click on square 2 for the second time but the square text does not change
+        cy.get("[data-cy='square']").eq(1).click().should('have.text', 'O')
+    })
+})
+
 describe('test game', () => {
-    it("should show turn text", () => {
+    before(() => {
+        cy.visit('http://localhost:3000/');
+    });
+
+    it('should be able to choose first player', () => {
+        selectPlayer('X');
+    })
+
+    it("should show game status", () => {
         cy.get("[data-cy='game-status']").should('be.visible')
     });
 
@@ -36,15 +80,30 @@ describe('test game', () => {
         cy.get("[data-cy='square']").should('have.length', 9)
     })
 
-    it('click on the squares', () => {
-        cy.get("[data-cy='square']").eq(0).click()
+    it('click a square should change turn text', () => {
+        cy.get("[data-cy='game-status']")
+        .invoke('text')
+        .then((text1) => {
+            cy.get("[data-cy='square']").eq(0).click()
+            cy.get("[data-cy='game-status']")
+            .invoke('text')
+            .should((text2) => {
+                expect(text1).not.to.eq(text2)
+            })
+        })
+
+    })
+
+
+    // Click squares until there is a winner
+    it('should be allowed to click on squares until there is a winner', () => {
         cy.get("[data-cy='square']").eq(1).click()
         cy.get("[data-cy='square']").eq(3).click()
         cy.get("[data-cy='square']").eq(4).click()
         cy.get("[data-cy='square']").eq(6).click()
     })
 
-    it("should show winner text", () => {
+    it("should show winner text if there is a winner", () => {
         cy.get("[data-cy='game-status']").contains('X won')
     });
 
@@ -53,5 +112,22 @@ describe('test game', () => {
             cy.get("[data-cy='square']").should('have.text', '')
         })
     })
+
+    // Click until all squares are filled but there is no winner
+    it('should allow click until all squares are filled and no one won ', () => {
+        cy.get("[data-cy='square']").eq(0).click()
+        cy.get("[data-cy='square']").eq(1).click()
+        cy.get("[data-cy='square']").eq(2).click()
+        cy.get("[data-cy='square']").eq(5).click()
+        cy.get("[data-cy='square']").eq(3).click()
+        cy.get("[data-cy='square']").eq(4).click()
+        cy.get("[data-cy='square']").eq(8).click()
+        cy.get("[data-cy='square']").eq(6).click()
+        cy.get("[data-cy='square']").eq(7).click()
+    })
+
+    it("should show tie text if no one won", () => {
+        cy.get("[data-cy='game-is-tie']").should('exist')
+    });
 })
 
